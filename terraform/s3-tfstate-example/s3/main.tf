d363601bb8e51c92193a0cfef8ca36f7ee470633
jrtripathy <jrtripathy@gmail.com>
2020-05-12 19:01:47 -0400

    Add files via upload

---
+++ terraform/s3-tfstate-example/s3/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "s3-tfstate-example" {
+    bucket = "terraform-s3-tfstate-example"
+    acl = "private"
+
+    versioning {
+      enabled = true
+    }
+
+    lifecycle {
+      prevent_destroy = true
+    }
+
+    tags {
+      Name = "Terraform S3 tfstate Example"
+      Org = "jrtripathy.com"
+    }      
+}
